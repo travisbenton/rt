2d5f1e8e3e02d2e79b7229a4c184ccdc585b28ff
Travis Benton <travis.benton@favordelivery.com>
2020-02-14 16:08:21 -0600

    flatten with async/await

--- index.js
+++ index.js
@@ -8,51 +8,46 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
-const fetchMovies = movie => {
-	fetch(`${ROOT}napi/search/?limit=5&query=${movie || process.argv[2]}`)
-	  .then(res => res.text())
-	  .then(body => {
-	  	const moviesArr = JSON.parse(body).movies
+const fetchMovies = async (movie) => {
+	const res = await fetch(`${ROOT}napi/search/?limit=5&query=${movie || process.argv[2]}`)
+	const body = await res.text()
+	const moviesArr = JSON.parse(body).movies
 
-	  	if (!moviesArr.length) {
-	  		return rl.question('Nothing found. Try again\n', movie => fetchMovies(movie))
-	  	}
+	if (!moviesArr.length) {
+		return rl.question('Nothing found. Try again\n', movie => fetchMovies(movie))
+	}
 
-			const movieNames = moviesArr
-				.map((movie, i) => `${i + 1}) ${movie.name} (${movie.year})`)
-				.concat(`${moviesArr.length + 1}) Try again`)
+	const movieNames = moviesArr
+		.map((movie, i) => `${i + 1}) ${movie.name} (${movie.year})`)
+		.concat(`${moviesArr.length + 1}) Try again`)
 
-			rl.question(`${movieNames.join('\n')}\n`, index => {
-				const indexAsNum = parseInt(index, 10)
+	rl.question(`${movieNames.join('\n')}\n`, index => {
+		const indexAsNum = parseInt(index, 10)
 
-				if (indexAsNum === moviesArr.length + 1) {
-					return rl.question('Ok, whatcha got?\n', movie => fetchMovies(movie))
-				}
+		if (indexAsNum === moviesArr.length + 1) {
+			return rl.question('Ok, whatcha got?\n', movie => fetchMovies(movie))
+		}
 
-				const url = moviesArr[indexAsNum - 1].url
-				const moviePage = fetch(`${ROOT}${url}`)
-					.then(res => res.text())
-					.then(body => {
-						const $ = cheerio.load(body)
-						const $pct = $('.mop-ratings-wrap__percentage')
+		const url = moviesArr[indexAsNum - 1].url
+		const res = await fetch(`${ROOT}${url}`)
+		const moviePage = await res.text()
+		const $ = cheerio.load(moviePage)
+		const $pct = $('.mop-ratings-wrap__percentage')
 
-						if (!$pct.length) {
-							console.log('Hold your horses! No score yet')
-							rl.close()
-						} else {
-							const score = +$pct.first().text().trim().replace('%', '')
-							rl.question('What score is considered passing?\n', num => {
-								console.log(score > num ? 'PASS 🎉' : 'FAIL 😢')
-								rl.question('Wanna spoil it? (y/n)\n', repl => {
-									if (repl === 'yes' || repl === 'y') console.log(`${score}%`)
-									rl.close()
-								})
-							})
-						}
-					})
+		if (!$pct.length) {
+			console.log('Hold your horses! No score yet')
+			rl.close()
+		} else {
+			const score = +$pct.first().text().trim().replace('%', '')
+			rl.question('What score is considered passing?\n', num => {
+				console.log(score > num ? 'PASS 🎉' : 'FAIL 😢')
+				rl.question('Wanna spoil it? (y/n)\n', repl => {
+					if (repl === 'yes' || repl === 'y') console.log(`${score}%`)
+					rl.close()
+				})
 			})
-
-	  })
+		}
+	})
 }
 
 fetchMovies()
